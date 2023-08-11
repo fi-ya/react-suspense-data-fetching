2ef6e1009c1611c7dcc42c52ffcc82cf4b5ab44a
fi-ya <69358550+fi-ya@users.noreply.github.com>
2023-08-11 12:22:00 +0100

    Create ProfileDetails & ProfilePosts components and wrap with Suspense and fallback prop

--- src/App.js
+++ src/App.js
@@ -8,9 +8,46 @@ const resource = fetchData();
 function App() {
   return (
     <div className="container my-5">
-      <h1>Hello World</h1>
+        <Suspense fallback={<h1>Loading User...</h1>}>
+            <ProfileDetails/>
+        </Suspense>
+        <Suspense fallback={<h1>Loading Posts...</h1>}>
+            <ProfilePosts/>
+        </Suspense>
     </div>
   );
+}
+
+const ProfileDetails = () => {
+    const user = resource.user.read()
+
+    return (
+        <div className='card card-body my-3'>
+            <h1 className='large text-primary'>{user.name}</h1>
+            <ul>
+                <li>Username: {user.username}</li>
+                <li>Email: {user.email}</li>
+                <li>City: {user.address.city}</li>
+            </ul>
+        </div>
+    );
+}
+
+const ProfilePosts = () => {
+    const posts = resource.posts.read()
+
+    return (
+        <ul className='list-group'>
+            <li className='list-group-item'>
+                <strong>Latest Posts</strong>
+            </li>
+            {posts.map(post => (
+                <li className='list-group-item' key={post.id}>
+                    {post.title}
+                </li>
+            ))}
+        </ul>
+    );
 }
 
 export default App;
